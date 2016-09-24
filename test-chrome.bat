908af3fc58724182b702738312ccc0d987e48f09
Adrian O'Grady <elpollouk@gmail.com>
2016-09-24 17:02:22 +0100

    Changed chrometemp to .chrometemp

--- test-chrome.bat
+++ test-chrome.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir="%cd%/chrometemp" --disable-web-security "%cd%/test/index.html"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir="%cd%/.chrometemp" --disable-web-security "%cd%/test/index.html"
